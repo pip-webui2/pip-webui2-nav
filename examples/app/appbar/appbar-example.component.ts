b2496a3654e89b539d736319ec1487b33fca982a
dvoikinalex <94dvoikin@gmail.com>
2017-10-04 20:05:43 +0300

    [Appbar example] update component and module

--- examples/app/appbar/appbar-example.component.ts
+++ examples/app/appbar/appbar-example.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import { PipNavPartService } from '../pip-webui2-nav';
 
 @Component({
   selector: 'appbar-example',
-  templateUrl: './appbar-example.component.html'
+  templateUrl: './appbar-example.component.html',
+  styleUrls: ['./appbar-example.component.scss']
 })
-export class AppBarExampleComponent { }
+export class AppBarExampleComponent {
+  constructor(
+    private service: PipNavPartService
+  ) { }
+
+  public appbarIconPartName: string = 'appbar-icon';
+  public isIconShown: boolean = true;
+
+  public ngOnInit() {
+    
+  }
+
+  public onToogleIcon(): void {
+    this.isIconShown = !this.isIconShown;
+    this.service.changeVisibility(this.appbarIconPartName, this.isIconShown);
+  }
+}
--- examples/app/appbar/appbar-example.module.ts
+++ examples/app/appbar/appbar-example.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MdButtonModule } from '@angular/material';
 
 import { PipAppbarModule, PipAppbarIconModule, PipNavPartModule } from '../pip-webui2-nav';
 import { AppBarExampleComponent } from './appbar-example.component';
+import { PipNavPartService } from '../pip-webui2-nav';
+
 
 @NgModule({
   declarations: [
     AppBarExampleComponent
   ],
   imports: [
+    BrowserModule,
+    MdButtonModule,
+
     PipAppbarModule,
     PipAppbarIconModule,
     PipNavPartModule
@@ -15,6 +22,8 @@ import { AppBarExampleComponent } from './appbar-example.component';
   exports: [
     AppBarExampleComponent
   ],
-  providers: [],
+  providers: [
+    PipNavPartService
+  ],
 })
 export class AppbarExampleModule { }
