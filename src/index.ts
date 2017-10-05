8f6b27ffb1f8727400cca7c29049f78b26f9f516
dvoikinalex <94dvoikin@gmail.com>
2017-10-05 18:44:26 +0300

    [Root index file] add primary actions

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './test/index';
 export * from './appbar/index';
 export * from './appbar-icon/index';
 export * from './appbar-breadcrumb/index';
+export * from './appbar-primary-actions/index';
 
 export * from './sidenav/index';
 export * from './sidenav-menu/index';
