26806aca8b03a10a1925083946cb354c34a368d2
dvoikinalex <94dvoikin@gmail.com>
2017-10-06 16:14:58 +0300

    [Appbar breadcrumb] update test

--- src/appbar-breadcrumb/appbar-breadcrumb.component.spec.ts
+++ src/appbar-breadcrumb/appbar-breadcrumb.component.spec.ts
@@ -1,25 +1,69 @@
-/*import { TestBed, inject } from '@angular/core/testing';
+import * as assert from 'assert';
+import { TestBed, inject, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-import { AppbarBreadcrumbComponent } from './appbar-breadcrumb.component';
+import { PipAppbarBreadcrumbModule } from './appbar-breadcrumb.module';
+import { PipNavPartService } from '../navpart/shared/navpart.service';
+import { PipAppbarBreadcrumbComponent } from './appbar-breadcrumb.component';
 
-describe('a appbar-breadcrumb component', () => {
-	let component: AppbarBreadcrumbComponent;
+describe('a appbar-breaPipAppbarBreadcrumbComponentdcrumb component', () => {
+	let component: PipAppbarBreadcrumbComponent;
+	let service: PipNavPartService;
+	let fixture: ComponentFixture<PipAppbarBreadcrumbComponent>;
+	let de: DebugElement;
+	let el: HTMLElement;
+	let navPartName = 'breadcrumb';
 
 	// register all needed dependencies
 	beforeEach(() => {
 		TestBed.configureTestingModule({
+			imports: [
+				PipAppbarBreadcrumbModule
+			],
 			providers: [
-				AppbarBreadcrumbComponent
+				PipAppbarBreadcrumbComponent,
+				PipNavPartService
 			]
 		});
 	});
 
 	// instantiation through framework injection
-	beforeEach(inject([AppbarBreadcrumbComponent], (AppbarBreadcrumbComponent) => {
-		component = AppbarBreadcrumbComponent;
+	beforeEach(inject([PipNavPartService], (navPartSErvice: PipNavPartService) => {
+		fixture = TestBed.createComponent(PipAppbarBreadcrumbComponent);
+		service = navPartSErvice;
+		component = fixture.componentInstance;
+
+		de = fixture.debugElement;
+		el = de.nativeElement;
 	}));
 
-	it('should have an instance', () => {
-		expect(component).toBeDefined();
+	it('should add and show titles', () => {
+		let props = { items: [
+			{ title: 'Title 1' }
+		]};
+
+		component.pipNavPartName = navPartName;
+		service.updatePartByName(navPartName, true, props);
+		expect(component.config).toEqual(props);
+		fixture.detectChanges();
+		let navItems = el.getElementsByClassName('navigation-item');
+
+		expect(navItems.length).toEqual(props.items.length);
+		expect(navItems[0].children[0].innerHTML).toEqual(props.items[0].title);
 	});
-});*/
+
+	it('should show search', () => {
+		let props = { items: [
+			{ title: 'Title 1' },
+		], searchCriteria: 'criteria'};
+
+		component.pipNavPartName = navPartName;
+		service.updatePartByName(navPartName, true, props);
+		fixture.detectChanges();
+		let navItems = el.getElementsByClassName('search-container');
+
+		expect(navItems.length).toEqual(2);
+		expect(navItems[0].innerHTML).toEqual(props.searchCriteria + ' - ');
+	});
+});
