b6d20de1687a81bf229a6065bf1eb36ff0f8414b
dvoikinalex <94dvoikin@gmail.com>
2017-10-06 12:51:24 +0300

    [Index root file] update

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './appbar/index';
 export * from './appbar-icon/index';
 export * from './appbar-breadcrumb/index';
 export * from './appbar-primary-actions/index';
+export * from './appbar-secondary-actions/index';
 
 export * from './sidenav/index';
 export * from './sidenav-menu/index';
