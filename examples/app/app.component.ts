0f8b17f6e04634d8da09bfcd4a221961c448af77
dvoikinalex <94dvoikin@gmail.com>
2017-10-17 19:22:36 +0300

    [Examples] update

--- examples/app/app.component.ts
+++ examples/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PipMediaService } from 'pip-webui2-layouts';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {
-    
+  constructor(
+    public media: PipMediaService
+  ) {
+    media.activate();
   }
 
   public list: any[] = [
--- examples/app/app.module.ts
+++ examples/app/app.module.ts
@@ -1,21 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule} from '@angular/router'
+import { Routes, RouterModule } from '@angular/router'
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { PipTestModule, PipAppbarIconModule } from './pip-webui2-nav';
-import { PipAppbarModule, PipSidenavModule } from 'pip-webui2-layouts';
-import { AppbarExampleModule } from './appbar/appbar-example.module';
-import { SidenavExampleModule } from './sidenav/sidenav-example.module';
-import { AppBarExampleComponent } from './appbar/appbar-example.component';
-import { SidenavExampleComponent } from './sidenav/sidenav-example.component'; 
-import { ExampleListModule } from './examples-list/examples-list.module';
+import { PipAppbarModule, PipAuxPanelModule, PipMediaModule, PipSidenavModule, PipContentLayoutModule, PipMainLayoutModule } from 'pip-webui2-layouts';
+import { PipAppbarBreadcrumbModule, PipAppbarIconModule, PipAppbarPrimaryActionsModule, PipAppbarSecondaryActionsModule, PipNavPartModule, PipSidenavHeaderModule, PipSidenavMenuModule } from './pip-webui2-nav';
+
+import { NavPartsExampleModule } from './navparts/navparts-example.module';
+import { NavPartsExampleComponent } from './navparts/navparts-example.component';
 import { AppComponent } from './app.component';
 
 const appRoutes: Routes = [
-  { path: 'appbar', component: AppBarExampleComponent },
-  { path: 'sidenav', component: SidenavExampleComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'appbar' }
+  { path: 'navparts', component: NavPartsExampleComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'navparts' }
 ];
 
 @NgModule({
@@ -26,10 +23,23 @@ const appRoutes: Routes = [
     BrowserModule,
     RouterModule,
     FlexLayoutModule,
-    PipTestModule,
-    AppbarExampleModule,
-    SidenavExampleModule,
-    ExampleListModule,
+
+    PipAppbarModule,
+    PipAuxPanelModule,
+    PipMediaModule,
+    PipSidenavModule, 
+    PipContentLayoutModule, 
+    PipMainLayoutModule,
+
+    PipAppbarBreadcrumbModule, 
+    PipAppbarIconModule, 
+    PipAppbarPrimaryActionsModule, 
+    PipAppbarSecondaryActionsModule, 
+    PipNavPartModule, 
+    PipSidenavHeaderModule, 
+    PipSidenavMenuModule,
+
+    NavPartsExampleModule,
 
     RouterModule.forRoot(appRoutes, { useHash: true })
   ],
@@ -37,4 +47,3 @@ const appRoutes: Routes = [
   bootstrap: [AppComponent]
 })
 export class AppModule { }
- 
