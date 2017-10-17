c7d663ec5c338323245d06b950319d3daee8cc30
dvoikinalex <94dvoikin@gmail.com>
2017-10-17 19:21:56 +0300

    [Nav part] update

--- src/navpart/navpart.directive.ts
+++ src/navpart/navpart.directive.ts
@@ -6,9 +6,14 @@ import { Subscription } from 'rxjs/Subscription';
 @Directive({ selector: '[pipNavPart]' })
 export class PipNavPartDirective {
     private subscription: Subscription;
+    private isVisible: boolean = false;
     @Input() public set pipNavPart(partName: string) {
         this.subscription = this.service.updatePartByName(partName, null, null).visible.subscribe(visible => {
-            visible ? this.viewContainer.createEmbeddedView(this.templateRef) : this.viewContainer.clear();
+            if (this.isVisible == true && visible == true) return;
+            else {
+                this.isVisible = visible;
+                visible ? this.viewContainer.createEmbeddedView(this.templateRef) : this.viewContainer.clear();
+            }
         });
     }
 
