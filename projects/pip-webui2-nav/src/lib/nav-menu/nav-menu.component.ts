3b38efca3042d212083a698e370313cf42bbb1ea
Aleksander Koshevierov <Iworb.ne@gmail.com>
2018-10-05 11:56:54 +0300

    Selection

--- projects/pip-webui2-nav/src/lib/nav-menu/nav-menu.component.ts
+++ projects/pip-webui2-nav/src/lib/nav-menu/nav-menu.component.ts
@@ -1,8 +1,9 @@
 import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 import { PipNavService } from '../shared/nav.service';
-import { NavMenuLink, NavMenuSection, NavMenuConfig } from './shared/nav-menu.model';
+import { NavMenuConfig } from './shared/nav-menu.model';
 
 @Component({
     selector: 'pip-nav-menu',
@@ -15,13 +16,16 @@ export class PipNavMenuComponent implements OnInit, OnDestroy {
 
     private subscription: Subscription;
     public config: NavMenuConfig;
+    public selectedSectionIndex: number;
     public selectedItemIndex: number;
     public sections: any[] = [];
 
     public constructor(
+        private router: Router,
         private service: PipNavService,
         private cd: ChangeDetectorRef
     ) {
+        this.selectedSectionIndex = 0;
         this.selectedItemIndex = 0;
     }
 
@@ -29,6 +33,21 @@ export class PipNavMenuComponent implements OnInit, OnDestroy {
         this.subscription = this.service.updateItemByName(this.partName, null).properties.subscribe((newConfig: NavMenuConfig) => {
             this.config = newConfig;
             if (this.config && this.config.sections) { this.sections = this.config.sections; }
+            if (this.sections && this.sections.length) {
+                const url = this.router.url.replace(/^\//, '');
+                const selectIndex = () => {
+                    for (let sk = 0; sk < this.sections.length; sk++) {
+                        for (let lk = 0; lk < this.sections[sk].links.length; lk++) {
+                            if (this.sections[sk].links[lk].url === url) {
+                                this.selectedSectionIndex = sk;
+                                this.selectedItemIndex = lk;
+                                return;
+                            }
+                        }
+                    }
+                };
+                selectIndex();
+            }
             this.cd.detectChanges();
         });
     }
